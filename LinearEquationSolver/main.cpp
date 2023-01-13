143a41685119295369bbf51ac922a47802a25894
Rafael Silva <76886825+Rafael-Nunes-Silva@users.noreply.github.com>
2023-01-13 18:14:45 -0300

    Update main.cpp

--- LinearEquationSolver/main.cpp
+++ LinearEquationSolver/main.cpp
@@ -2,17 +2,6 @@
 #include <string>
 #include "StringMathSolver.hpp"
 
-/*
-Programa que calcula equações feito como um desafio:
-Deve receber como input uma única string como: "x = 3 + 5", "x = PI * 2^2" ou "x = (40 + 20) * sqrt(7)"
-
-A resposta do programa deve ser também uma única string como: "x = 8", "x = 12,5" ou "x = 158,75"
-
-Opcionais:
-Dar a opção de demonstrar a resolução passo a passo
-*/
-
-
 /* Inputs de teste                   | resultado esperado
 * 6(2x + 3) + x - 7 = 3(5x + 7) + 2x | x = -2.5
 * x = (1 + 3 * 5) / (1 + 3)          | x = 4
